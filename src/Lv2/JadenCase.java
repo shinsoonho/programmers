977321bd205524c6c395db6fa5d4e8885c0558ae
shinsoonho <tnsghek12@naver.com>
2023-08-22 23:39:31 +0900

    JadenCase 문제풀이

---
+++ src/Lv2/JadenCase.java
@@ -0,0 +1,29 @@
+package Lv2;
+
+public class JadenCase {
+
+    private static String solution(String s) {
+
+        StringBuilder sb = new StringBuilder();
+
+        boolean upper = true;   // 첫 문자 대문자인지 판별(0번인덱스 대문자)
+
+        for (char ch : s.toCharArray()) {
+            sb = upper ? sb.append(Character.toUpperCase(ch)) : sb.append(Character.toLowerCase(ch));
+            upper = ch == ' ' ? true : false;   // ch가 공백이면 대문자, 아니면 소문자
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+
+        String s1 = "3people UnFollowed   me";
+        String s2 = "for the last week";
+
+        System.out.println(solution(s1));
+        System.out.println(solution(s2));
+
+    }
+
+}
