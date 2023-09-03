9ba167f096e26bd92a3d83964eec0701d5e9509a
shinsoonho <tnsghek12@naver.com>
2023-09-03 13:06:14 +0900

    다음 큰 숫자 - 문제풀이(binary)

---
+++ src/Lv1/문자열내림차순으로배치/DescString.java
@@ -0,0 +1,28 @@
+package Lv1.문자열내림차순으로배치;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class DescString {
+
+    private static String solution(String str) {
+
+        StringBuilder sb = new StringBuilder();
+
+        String[] ch = str.split("");
+        Arrays.sort(ch, Collections.reverseOrder());
+
+        for (String c : ch) {
+            sb.append(c);
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(solution("Zbcdefg"));
+
+    }
+
+}
